3542b94b53f35cfea71a9ad41542d492d7ae51e6
qiweiyu <qiweiyu@qtumdeMacBook-Pro.local>
2018-07-05 15:18:07 +0800

    update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
     <v-toolbar color="grey darken-3" dark fixed>
-      <v-toolbar-title @click="jumpTo('Index')">QTUMX </v-toolbar-title>
-      <v-subheader>One Click Launch Chain</v-subheader>
+      <v-toolbar-title @click="jumpTo('Index')">NUWA</v-toolbar-title>
+      <v-subheader>One Click One Chain</v-subheader>
       <v-spacer></v-spacer>
-      <template v-if="user">
-        <v-subheader>Hello, {{ user.address }}</v-subheader>
-        <v-btn color="red" @click="logout">Logout</v-btn>
-      </template>
-      <template v-else>
-        <v-btn color="primary" @click="jumpTo('Login')">Login</v-btn>
-      </template>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat href="https://github.com/qtumproject/qtum-enterprise/releases" target="_blank">Download</v-btn>
+        <v-btn flat href="https://docs.qtum.org/zh/One-Click-Launch-Chain/" target="_blank">Document(Chinese)</v-btn>
+        <v-btn flat href="mailto:x@qtum.org" target="_blank">Email</v-btn>
+        <template v-if="user">
+          <!--v-subheader>Hello, {{ user.address }}</v-subheader-->
+          <v-btn flat color="red" @click="logout">Logout</v-btn>
+        </template>
+        <template v-else>
+          <v-btn flat @click="jumpTo('Login')" color="green">Login</v-btn>
+        </template>
+      </v-toolbar-items>
     </v-toolbar>
   </div>
 </template>
